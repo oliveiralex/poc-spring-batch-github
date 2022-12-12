136a27fd69dadf65bc5d98b48556d677f0e46a19
Alexandre <alexandre.si.ufu@gmail.com>
2022-12-12 10:52:41 -0300

    project created

---
+++ src/test/java/br/com/oliveiralex/springbatchv5/SpringBatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oliveiralex.springbatchv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
